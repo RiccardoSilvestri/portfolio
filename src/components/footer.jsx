76fb2ab9aa07861db40b16ea685175219acf9a3a
Riccardo Silvestri <r.silvestri04@gmail.com>
2024-10-06 21:09:56 +0200

    ci:updated customization

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,8 @@
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 text-white p-4">
-        <div className="container mx-auto">
-          <p>instagram:riccardo.silvestri.it</p>
-        </div>
-      </footer>
-    );
+      <div>
+        <p>instagram:riccardo.silvestri.it</p>
+      </div>
+    )
   };
   export default Footer;
