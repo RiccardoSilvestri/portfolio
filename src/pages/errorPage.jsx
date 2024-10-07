bac89493d93cb3a917e77b82166d9d3348ea8668
RiccardoSilvestri <r.silvestri04@gmail.com>
2024-10-07 17:11:15 +0200

    ci:fixxed copy clipboard

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -1,7 +1,11 @@
+import Space from '../components/background.jsx'
+
 const ErrorPage = () => {
     return (
       <div>
-        <p>404</p>
+        <Space>
+        <p className="Dissolvenza min-h-screen flex items-start justify-center mt-20 text-white font-sans text-3xl">Error 404</p>
+        </Space>
       </div>
     );
   };
